1bd9f098f798ebf2fb974c92beea0690925af850
jdkim <jdkim@jdkimui-MacBookAir.local>
2023-01-20 15:18:52 +0900

    redirect logic added

--- next.config.js
+++ next.config.js
@@ -1,6 +1,16 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+
+  async redirects() {
+    return [
+      {
+        source: '/posts/:id',
+        destination: 'https://www.google.com/',
+        permanent: true,
+      },
+    ]
+  }
 }
 
 module.exports = nextConfig
